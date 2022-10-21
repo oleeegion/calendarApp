825e808083123ef8942c095a8171052b4002c778
oleeegion <timofeeva_2001@mail.ru>
2022-10-21 20:40:34 +0600

    help

--- app/src/main/java/com/example/calendar/MainActivity.kt
+++ app/src/main/java/com/example/calendar/MainActivity.kt
@@ -20,17 +20,56 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //метод для вызова экрана
 
-        dateTV = findViewById(R.id.idDate)
+        // инициализация переменных представления списка с их идентификаторами.
+        dateTV = findViewById(R.id.idTVDate)
         calendarView = findViewById(R.id.idCalendarView)
 
-
+        // в строке ниже мы добавляем set on
+        // прослушиватель изменения даты для просмотра календаря.
         calendarView.setOnDateChangeListener(
-            OnDateChangeListener {view, year, month, dayOfMonth ->
-                val date = (dayOfMonth.toString() + "." + (month + 1) + "." + year)
-                dateTV.setText(date)
+            OnDateChangeListener { view, year, month, dayOfMonth ->
+                // В этом Listener мы получаем значения например, год, месяц и день месяца
+                // в строке ниже мы создаем переменную в который мы добавляем все переменные в нем.
+                val Date = (dayOfMonth.toString() + "-" + (month + 1) + "-" + year)
+
+                // установите эту дату в TextView для отображения
+                dateTV.setText(Date)
             })
 
 
     }
 }
 
+
+
+//    class MainActivity : AppCompatActivity() {
+//
+//
+//        override fun onCreate(savedInstanceState: Bundle?) {
+//            super.onCreate(savedInstanceState)
+//            setContentView(R.layout.activity_main)
+
+
+//            // инициализация переменных
+//            // представления списка с их идентификаторами.
+//            dateTV = findViewById(R.id.idTVDate)
+//            calendarView = findViewById(R.id.idCalendarView)
+//
+//
+//            // в строке ниже мы добавляем set on
+//            // прослушиватель изменения даты для просмотра календаря.
+//            calendarView.setOnDateChangeListener(
+//                    OnDateChangeListener { view, year, month, dayOfMonth ->
+//                        // В этом Listener мы получаем значения
+//                        // например, год, месяц и день месяца
+//                        // в строке ниже мы создаем переменную
+//                        // в который мы добавляем все переменные в нем.
+//                        val Date = (dayOfMonth.toString() + "-"
+//                                + (month + 1) + "-" + year)
+//
+//                        // установите эту дату в TextView для отображения
+//                        dateTV.setText(Date)
+//                    })
+//        }
+//    }
+//}
